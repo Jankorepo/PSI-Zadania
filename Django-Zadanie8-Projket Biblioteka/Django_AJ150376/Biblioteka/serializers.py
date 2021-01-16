35f2c5bb02d8f64ed15f1f8072d0bed95cb2bd9d
Jankorepo <44462192+Jankorepo@users.noreply.github.com>
2021-01-16 19:01:29 +0100

    Testy i prawa dostępu

---
+++ Django-Zadanie8-Projket Biblioteka/Django_AJ150376/Biblioteka/serializers.py
@@ -0,0 +1,62 @@
+from rest_framework.serializers import ModelSerializer, ValidationError, HyperlinkedModelSerializer,\
+    HyperlinkedRelatedField, SlugRelatedField, ReadOnlyField
+from .models import Ksiazka, Gatunek, Czytelnik, HistoriaWypozyczen, AktualneWypozyczenia
+import datetime
+from django.contrib.auth.models import User
+
+class KsiazkaSerializer(ModelSerializer):
+    gatunek = SlugRelatedField(queryset=Gatunek.objects.all(), slug_field='kategoria')
+    ksiazke_dodal = ReadOnlyField(source='owner.username')
+    class Meta:
+        model= Ksiazka
+        fields = ['pk',"idKsiazka","url", "tytul", "autor", "minimalny_wiek_czytelnika", "data_wydania",
+                  "gatunek","czy_wypozyczona", 'ksiazke_dodal']
+    def validate_data_wydania(self, value):
+        data=self.get_initial()
+        if datetime.datetime.strptime(data.get("data_wydania"),"%Y-%m-%d").date()>datetime.date.today():
+            raise ValidationError("Data musi być późniejsza niż dzieiejszy dzień")
+        return value
+class GatunekSerializer(HyperlinkedModelSerializer):
+    ksiazki = HyperlinkedRelatedField(many=True, read_only=True, view_name='ksiazka-detail')
+    class Meta:
+        model = Gatunek
+        fields = [ 'pk', 'kategoria', 'ksiazki']
+
+class CzytelnikSerializer(HyperlinkedModelSerializer):
+    aktualnieWypozyczoneKsiazki=HyperlinkedRelatedField(many=True, read_only=True, view_name='aktualnewypozyczenia-detail')
+    historia=HyperlinkedRelatedField(many=True, read_only=True, view_name="historiawypożyczeń-detail")
+    czytelnika_dodal = ReadOnlyField(source='owner.username')
+    class Meta:
+        model= Czytelnik
+        fields = ['pk', 'url',"idCzytelnik","imie" ,"nazwisko" ,"wiek" ,"adres","telefon", 'czytelnika_dodal',
+                  "aktualnieWypozyczoneKsiazki", "historia"]
+
+class AktualneWypozyczeniaSerializer(ModelSerializer):
+    czytelnik=SlugRelatedField(queryset=Czytelnik.objects.all(), slug_field="nazwisko")
+    ksiazka = SlugRelatedField(queryset=Ksiazka.objects.all(), slug_field="tytul")
+    class Meta:
+        model = AktualneWypozyczenia
+        fields = ['url',"idWypozyczenia","data_wypozyczenia" ,"ksiazka" ,"czytelnik",]
+class HistoriaWypozyczenSerializer(ModelSerializer):
+    czytelnik = SlugRelatedField(queryset=Czytelnik.objects.all(), slug_field="nazwisko")
+    ksiazka = SlugRelatedField(queryset=Ksiazka.objects.all(), slug_field="tytul")
+    class Meta:
+        model = HistoriaWypozyczen
+        fields = ["idZwrot", "data_wypozyczenia", "data_zwrotu" ,"ksiazka" ,"czytelnik",]
+
+class PracownikDodaneKsiazkiSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Ksiazka
+        fields = ['idKsiazka','tytul' ]
+
+class PracownikDodaniCzytelnicySerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Czytelnik
+        fields = ['idCzytelnik','imie', 'nazwisko' ]
+
+class PracownikSerializer(HyperlinkedModelSerializer):
+    dodane_ksiazki=PracownikDodaneKsiazkiSerializer(many=True,read_only=True)
+    dodani_czytelnicy = PracownikDodaniCzytelnicySerializer(many=True, read_only=True)
+    class Meta:
+        model = User
+        fields = ['pk','username','dodane_ksiazki','dodani_czytelnicy']
---
+++ Django-Zadanie8-Projket Biblioteka/Django_AJ150376/Biblioteka/tests.py
@@ -0,0 +1,83 @@
+from rest_framework.test import APITestCase
+from . import views
+from .models import Ksiazka, Gatunek, Czytelnik
+from rest_framework import status
+from rest_framework.reverse import reverse
+from django.utils.http import urlencode
+from django import urls
+import datetime
+
+
+class GatunekTests(APITestCase):
+    def post_gatunek(self, kategoria):
+        url = reverse(views.GatunekList.name)
+        data = {'kategoria': kategoria}
+        response = self.client.post(url, data, format='json')
+        return response
+
+    def test_post_and_get_book_category(self):
+        kategoria = 'nowy gatunek'
+        response = self.post_gatunek(kategoria)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Gatunek.objects.count() == 1
+        assert Gatunek.objects.get().kategoria == kategoria
+
+    def test_update_book_category(self):
+        kategoria = 'nowy gatunek'
+        response = self.post_gatunek(kategoria)
+        url = urls.reverse(views.GatunekDetail.name,None,{response.data['pk']})
+        zmieniona_kategoria = 'inny nowy gatunek'
+        data = {'kategoria': zmieniona_kategoria}
+        patch_response = self.client.patch(url, data, format='json')
+        assert patch_response.status_code == status.HTTP_200_OK
+        assert patch_response.data['kategoria'] == zmieniona_kategoria
+
+    def test_get_book_category(self):
+        kategoria = 'nowy gatunek'
+        response = self.post_gatunek(kategoria)
+        url = urls.reverse(views.GatunekDetail.name,None,{response.data['pk']})
+        get_response = self.client.patch(url, format='json')
+        assert get_response.status_code == status.HTTP_200_OK
+        assert get_response.data['kategoria'] == kategoria
+
+class CzytelnikTests(APITestCase):
+    def post_czytelnik(self, imie, nazwisko, wiek, adres, telefon, owner):
+        url = reverse(views.CzytelnikList.name)
+        data = {'imie': imie,'nazwisko': nazwisko, 'wiek': wiek,'adres': adres,
+                'telefon': telefon, 'owner': owner}
+        response = self.client.post(url, data, format='json')
+        return response
+    def test_post_and_get_czytelnik(self):
+        imie = 'Jan'
+        nazwisko= "Wiśniewski"
+        wiek= 81
+        adres = "Wojska Polskiego 8/1"
+        telefon = '123456789'
+        owner = 1
+        response = self.post_czytelnik(imie, nazwisko, wiek, adres, telefon, owner)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Czytelnik.objects.count() == 1
+        assert Czytelnik.objects.get().imie == imie
+        assert Czytelnik.objects.get().nazwisko == nazwisko
+        assert Czytelnik.objects.get().wiek == wiek
+        assert Czytelnik.objects.get().adres == adres
+        assert Czytelnik.objects.get().telefon == telefon
+
+    def test_update_czytelnik(self):
+        imie = 'Jan'
+        nazwisko = "Wiśniewski"
+        wiek = 81
+        adres = "Wojska Polskiego 8/1"
+        telefon = '123456789'
+        owner = 1
+        response = self.post_czytelnik(imie, nazwisko, wiek, adres, telefon, owner)
+        url = urls.reverse(views.CzytelnikDetail.name, None, {response.data['pk']})
+        nowy_adres = "Wojska Polskiego 8/1"
+        nowy_telefon = '123456789'
+        data = {'imie': imie, 'nazwisko': nazwisko, 'wiek': wiek, 'adres': nowy_adres,
+                'telefon': nowy_telefon, 'owner': owner}
+        patch_response = self.client.patch(url, data, format='json')
+        assert patch_response.status_code == status.HTTP_200_OK
+        assert patch_response.data['telefon'] == nowy_telefon
+        assert patch_response.data['adres'] == nowy_adres
+
